5aa645a8205d7439c6a64c1a1d52f3cf40810841
wesleywchang <3734594+wesleywchang@users.noreply.github.com>
2022-12-20 10:48:31 -0800

    markdown output formatting

--- scraper_tools.py
+++ scraper_tools.py
@@ -5,7 +5,8 @@ from markdownify import markdownify as md
 AOC_URL = 'https://adventofcode.com'
 
 def download_day(year, day, auth_token) -> tuple:
-    raw = requests.get(f'{AOC_URL}/{year}/day/{day}', timeout=3)
+    url = f'{AOC_URL}/{year}/day/{day}'
+    raw = requests.get(url, timeout=3)
 
     if raw.status_code != 200:
         print(f'AOC {year} Day {day} could not be downloaded')
@@ -13,10 +14,14 @@ def download_day(year, day, auth_token) -> tuple:
     
     soup = BeautifulSoup(raw.content, 'html.parser')
     article = md(str(soup.find('article')))
+    article = article.split('\n')
+    article[0] = '#' + article[0].replace('-', '')
+    article[1] = f'> [adventofcode.com/{year}/day/{day}](url)'
+    content = '\n'.join(article)
 
     if auth_token is not None:
         try:
-            raw = requests.get(f'{AOC_URL}/{year}/day/{day}/input', timeout=3, cookies={'session': auth_token}).content
+            raw = requests.get(f'{url}/input', timeout=3, cookies={'session': auth_token}).content
             data = str(raw.decode('utf8'))
 
         except:
@@ -25,7 +30,7 @@ def download_day(year, day, auth_token) -> tuple:
     else:
         data = None
 
-    return (article, data)
+    return (content, data)
 
 def download_year(year, auth_token, start=1, stop=26) -> list:
     challenges = {}
